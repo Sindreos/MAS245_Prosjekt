278c90657ed6e54419393daec4f33dd238ee351d
Sindreos <Sindreolsen@msn.com>
2022-10-26 16:35:23 +0200

    Sending CAN message to Arduino

--- Del_2/Send_CAN_mld/Send_CAN_mld.ino
+++ Del_2/Send_CAN_mld/Send_CAN_mld.ino
@@ -1,52 +1,132 @@
+
 #include <FlexCAN.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#include <Fonts/FreeMono9pt7b.h>
+const int JOY_LEFT = 18;
+const int JOY_RIGHT = 17;
+const int JOY_CLICK =19;
+const int JOY_UP  = 22;
+const int JOY_DOWN = 23;
 
-#define OLED_DC     6 //Choose pin for OLED Screen
+#define OLED_DC     6
 #define OLED_CS     10
 #define OLED_RESET  5
-#define SSD1306_LCDHEIGHT 64 //Redefine the LCD height from 32 to 64
-Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS); 
+Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
 
-static CAN_message_t mes, rmes; //Buffer structure
+#define SSD1306_LCDHEIGHT 64
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+static CAN_message_t msg,rxmsg;
+volatile uint32_t count = 0;
+IntervalTimer TX_timer;
+String CANStr(""); 
+volatile uint32_t can_msg_count = 0;
 
 void setup(){
-  display.begin(SSD1306_SWITCHCAPVCC); 
-  display.clearDisplay(); //Clears the buffer of the display
   
-  Can0.begin(250000); //Sets the Baud Rate of the can0 bus (250000 is standard) 
-  Can1.begin(250000); //Sets the Baud Rate of the can1 bus Teensy 3.6 have two CAN
-  mes.len = 8; //Sets the number of data bytes for this frame
-  mes.buf[0] = 1; //This is the "Payload", first data byte of the buffer
-  mes.buf[1] = 0;
-  mes.buf[2] = 1;
-  mes.buf[3] = 0;
-  mes.buf[4] = 1;
-  mes.buf[5] = 0;
-  mes.buf[6] = 1;
-  mes.buf[7] = 0;
-  mes.buf[8] = 1;
-  mes.flags.remote = 0; //Sets the frame as normal, if 1 then frame is remote request frame
-  mes.id = 3046; //Specify an id
-  mes.flags.extended = 0; //Decides if it uses the extended bit id or regular (up to 4095)
-  Can0.write(mes);
-  Can1.write(mes); 
-  }
+  Can0.begin(500000); //set speed here. 
+  Can1.begin(500000); //set speed here.  
+  
+  pinMode(JOY_LEFT, INPUT_PULLUP);
+  pinMode(JOY_RIGHT, INPUT_PULLUP);
+  pinMode(JOY_CLICK, INPUT_PULLUP);
+  pinMode(JOY_UP, INPUT_PULLUP);
+  pinMode(JOY_DOWN, INPUT_PULLUP); 
+  
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC);
+  // Clear the buffer.
+  display.clearDisplay();
+  
+  delay(1000);
+  Serial.println(F("CAN Bus Tx test"));
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(10,16);
+  display.println(" Testing");
+  display.display();
+
+  delay(1000);
+  
+  display.clearDisplay();
+  
+  msg.buf[0] = 1;
+  msg.buf[1] = 2;
+  msg.buf[2] = 0;
+  msg.buf[3] = 0;
+  msg.buf[4] = 0;
+  msg.buf[5] = 0;
+  msg.buf[6] = 0;
+  msg.len = 8;
+  msg.id = 0x7DF;
+  msg.flags.extended = 0; 
+  msg.flags.remote = 0;
+//  msg.timeout = 500;
+  TX_timer.begin(tx_CAN, 1000000);    /* Start interrutp timer */
+  
+ 
+}
+
+/* From Timer Interrupt */
+void tx_CAN(void)
+{
+  msg.len = 8;
+  msg.id = 0x7DF;
+  msg.buf[0] = 0x02;
+  msg.buf[1] = 0x01;
+  msg.buf[2] = 0x0c;
+  msg.buf[3] = 0;
+  msg.buf[4] = 0;
+  msg.buf[5] = 0;
+  msg.buf[6] = 0;
+  msg.buf[7] = 0;
+  /*
+  msg.buf[4] = can_msg_count >> 24;
+  msg.buf[5] = can_msg_count >> 16;
+  msg.buf[6] = can_msg_count >> 8;
+  msg.buf[7] = can_msg_count;
+  */
+  Can0.write(msg);
+  Can1.write(msg);
+  can_msg_count++;
+}
 
 void loop() {
-     Can0.read(rmes)
-     Serial.print(rmes.id,HEX); 
+ 
+  int i;
+  
+  while(Can0.read(rxmsg))
+  { 
+     String CANStr(""); 
+     for (int i=0; i < 8; i++) {     
+         CANStr += (" ") ;
+         CANStr += String(rxmsg.buf[i],HEX);
+         
+     }
+     display.setCursor(1,1);
+     Serial.print(rxmsg.id,HEX); 
      Serial.print(' '); 
-     Serial.print(rmes.len,HEX); 
-     Serial.print(' ');  
+     Serial.print(rxmsg.len,HEX); 
+     Serial.print(' ');
+     Serial.println(CANStr);  
      
-     display.fillRect(0,15,128, 30,BLACK);
-     display.setCursor(0,15);
+     display.fillRect(0,0,128, 32, WHITE);  //
+     display.fillRect(1,1,126, 30, BLACK);  //
      
-     display.println(rmes.id,HEX); 
-     display.println(rmes.len,HEX); 
+     
+     display.println(rxmsg.id,HEX); 
+     display.print(" "); 
+     display.println(rxmsg.len,HEX); 
+     display.println(CANStr);
      display.display();
+
+     
+  }
 }
