7676bb67bf425b9eb19d35e5acdb62a1d1d4f80a
Sindre Olsen <Sindreolsen@msn.com>
2022-11-02 12:32:10 +0100

    Added multiplayer functionality

--- Del_2/Ping_pong/Ping_pong.ino
+++ Del_2/Ping_pong/Ping_pong.ino
@@ -12,6 +12,7 @@
 #define OLED_CS     10
 #define OLED_RESET  5
 static CAN_message_t rmsg;
+static CAN_message_t msg;
 Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
 
 #if (SSD1306_LCDHEIGHT != 64)
@@ -35,44 +36,51 @@ display.setTextColor(WHITE);
 pinMode(JOY_UP, INPUT_PULLUP);
 pinMode(JOY_DOWN, INPUT_PULLUP);  
 display.clearDisplay();
-Player1.isP1 = false;   
-Player2.isP1 = true; 
+Player1.isP1 = true;   
+Player2.isP1 = false; 
 display.setFont(&Picopixel);
+msg.len = 8;
+msg.id = 002;
 }
 
 void loop() {
   display.clearDisplay();
   Player1.PadPos(UP, DOWN);
   Can0.read(rmsg);
-  Player2.PadPos(rmsg.buf[0], rmsg.buf[1]);
+  Player2.PadPos(rmsg.buf[2], rmsg.buf[3]);
   Eng.Ball(startTime);
 if(Eng.x < -4)
 {
-  countLeft++;
+  countRight++;
   Eng.x = 64;
   Eng.y = 32;
   Eng.u = 50;
-  delay(1000);
 }  
 if(Eng.x > 130)
 {
-  countRight++;  
+  countLeft++;  
   Eng.x = 64;
   Eng.y = 32;
   Eng.u = 50;
-  delay(1000);
 }  
   Eng.ColP1(Player1.y, Player1.lastPressed);
   Eng.ColP2(Player2.y, Player2.lastPressed);
-  display.fillCircle(Eng.x, Eng.y, 2, WHITE);  
+  display.fillCircle(Eng.x, Eng.y, 2, WHITE);
+  msg.buf[4] = Eng.x;
+  msg.buf[5] = Eng.y;
+  msg.buf[0] = digitalRead(JOY_UP);
+  msg.buf[1] = digitalRead(JOY_DOWN);
+  msg.buf[6] = countLeft;
+  msg.buf[7] = countRight;
+  Can0.write(msg);
   display.fillRect(126,Player1.y/10,4, Player1.length,WHITE);
   display.fillRect(0,Player2.y/10,2, Player2.length,WHITE);
   display.drawRoundRect(52, 0, 24, 9, 2, WHITE);   
   display.drawLine(64, 0, 64, 8, WHITE); 
-  Serial.print(rmsg.buf[0]);
   display.setCursor(67, 6);
   display.print(countRight);
   display.setCursor(56, 6);
   display.print(countLeft);
   display.display();
+  Serial.print(msg.buf[0]);
 }
